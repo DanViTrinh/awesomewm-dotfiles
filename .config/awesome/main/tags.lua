d8d42833fee85799c1af018615883973b24c69c8
DanVi007 <danvitrinh@outlook.com>
2023-04-08 18:05:58 +0200

    tags

--- .config/awesome/main/tags.lua
+++ .config/awesome/main/tags.lua
@@ -5,22 +5,32 @@ local _M = {}
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-function _M.get ()
-  tags = {}
+function _M.get()
+    local tags = {}
 
-  awful.screen.connect_for_each_screen(function(s)
-    -- Each screen has its own tag table.
-    tags[s] = awful.tag(
-      { "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, RC.layouts[1]
-    )
-  end)
-  
-  return tags
+    local tagpairs = {
+        names  = {
+            "❶", "❷", "❸",
+            "❹", "❺",
+            "❻", "break ❼", "chat ❽", "music ❾" },
+        layout = {
+            RC.layouts[2], RC.layouts[2], RC.layouts[2],
+            RC.layouts[2], RC.layouts[2], RC.layouts[2],
+            RC.layouts[2], RC.layouts[2], RC.layouts[2]
+        }
+    }
+
+    awful.screen.connect_for_each_screen(function(s)
+        -- Each screen has its own tag table.
+        tags[s] = awful.tag(tagpairs.names, s, tagpairs.layout)
+    end)
+
+    return tags
 end
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 return setmetatable(
-  {}, 
-  { __call = function(_, ...) return _M.get(...) end }
+    {},
+    { __call = function(_, ...) return _M.get(...) end }
 )
