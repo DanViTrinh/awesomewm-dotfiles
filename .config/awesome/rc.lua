f4cf3a929636ce1e79a71fdbe127dc74b5aebb9c
DanVi007 <danvitrinh@outlook.com>
2023-04-08 19:15:07 +0200

    menu

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -30,9 +30,10 @@ beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 vars = require("main.user-variables")
 -- This is used later as the default terminal and editor to run.
-terminal = RC.vars.terminal
-editor = RC.vars.editor
-editor_cmd = terminal .. " -e " .. editor
+-- previous
+-- terminal = RC.vars.terminal
+-- editor = RC.vars.editor
+-- editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
@@ -55,30 +56,36 @@ RC.layouts = main.layouts
 -- Tags
 RC.tags = main.tags()
 
+-- Menu
+RC.mainmenu = awful.menu({items = main.menu()})
+RC.launcher = awful.widget.launcher(
+    {image = beautiful.awesome_icon, menu = RC.mainmenu}
+)
+menubar.utils.terminal = RC.vars.terminal
 
 -- {{{ Menu
 -- Create a launcher widget and a main menu
-myawesomemenu = {
-    { "hotkeys",     function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-    { "manual",      terminal .. " -e man awesome" },
-    { "edit config", editor_cmd .. " " .. awesome.conffile },
-    { "restart",     awesome.restart },
-    { "quit",        function() awesome.quit() end },
-}
+-- myawesomemenu = {
+--     { "hotkeys",     function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
+--     { "manual",      terminal .. " -e man awesome" },
+--     { "edit config", editor_cmd .. " " .. awesome.conffile },
+--     { "restart",     awesome.restart },
+--     { "quit",        function() awesome.quit() end },
+-- }
 
-mymainmenu = awful.menu({
-    items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-        { "open terminal", terminal }
-    }
-})
+-- mymainmenu = awful.menu({
+--     items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
+--         { "open terminal", terminal }
+--     }
+-- })
 
-mylauncher = awful.widget.launcher({
-    image = beautiful.awesome_icon,
-    menu = mymainmenu
-})
+-- mylauncher = awful.widget.launcher({
+--     image = beautiful.awesome_icon,
+--     menu = mymainmenu
+-- })
 
--- Menubar configuration
-menubar.utils.terminal = terminal -- Set the terminal for applications that require it
+-- -- Menubar configuration
+-- menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 -- }}}
 
 -- Keyboard map indicator and switcher
@@ -203,7 +210,7 @@ end)
 
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
-    awful.button({}, 3, function() mymainmenu:toggle() end),
+    awful.button({}, 3, function() RC.mainmenu:toggle() end),
     awful.button({}, 4, awful.tag.viewnext),
     awful.button({}, 5, awful.tag.viewprev)
 ))
@@ -232,7 +239,7 @@ globalkeys = gears.table.join(
         end,
         { description = "focus previous by index", group = "client" }
     ),
-    awful.key({ modkey, }, "w", function() mymainmenu:show() end,
+    awful.key({ modkey, }, "w", function() RC.mainmenu:show() end,
         { description = "show main menu", group = "awesome" }),
 
     -- Layout manipulation
