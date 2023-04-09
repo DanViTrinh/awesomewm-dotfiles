cd78ba308a38c8c22506d32fc01341ee27f9980d
DanVi007 <danvitrinh@outlook.com>
2023-04-09 17:14:07 +0200

    linting

--- .config/awesome/main/signals.lua
+++ .config/awesome/main/signals.lua
@@ -29,8 +29,11 @@ client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
 end)
 
-client.connect_signal("focus", function(c) 
-  c.border_color = beautiful.border_focus 
+client.connect_signal("focus", function(c)
+  c.border_color = beautiful.border_focus
   c.border_width = beautiful.border_width
 end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+
+client.connect_signal("unfocus", function(c)
+  c.border_color = beautiful.border_normal
+end)
