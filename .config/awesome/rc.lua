b91105cb3c20fd54aeb6ddfec3e153f4b1ce867d
DanVi007 <danvitrinh@outlook.com>
2023-04-09 15:59:16 +0200

    signals

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -424,21 +424,10 @@ root.keys(globalkeys)
 -- Rules
 awful.rules.rules = main.rules(clientkeys,clientbuttons)
 
+-- Signals
+require("main.signals")
+
 -- {{{ Signals
--- Signal function to execute when a new client appears.
-client.connect_signal("manage", function(c)
-    -- Set the windows at the slave,
-    -- i.e. put it at the end of others instead of setting it master.
-    -- if not awesome.startup then awful.client.setslave(c) end
-
-    if awesome.startup
-        and not c.size_hints.user_position
-        and not c.size_hints.program_position then
-        -- Prevent clients from being unreachable after screen count changes.
-        awful.placement.no_offscreen(c)
-    end
-end)
-
 -- Add a titlebar if titlebars_enabled is set to true in the rules.
 client.connect_signal("request::titlebars", function(c)
     -- buttons for the titlebar
@@ -483,23 +472,12 @@ client.connect_signal("request::titlebars", function(c)
     }
 end)
 
--- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-    c:emit_signal("request::activate", "mouse_enter", { raise = false })
-end)
 
 -- TODO: Test: remove the below line and set properly
+-- Boder color and width
 beautiful.border_focus = "#eb3486"
+beautiful.border_width = 1.5
 
--- Default
--- client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
--- TODO: Modified
-client.connect_signal("focus", function(c)
-    c.border_color = beautiful.border_focus
-    c.border_width = 1.5
-end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
--- }}}
 
 -- Autostart Applications
 awful.spawn.with_shell("picom")
