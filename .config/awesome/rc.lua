e31997148033790937cae4989b33d7988f51a3ec
DanVi007 <danvitrinh@outlook.com>
2023-04-16 22:27:52 +0200

    fixed autofocus problem between clients

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -88,11 +88,16 @@ awful.rules.rules = main.rules(
 -- Signals
 require("main.signals")
 
--- helper thingy
+-- Other requirements
 local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
+
+-- autofocus clients
+require("awful.autofocus")
+
+
 
 -- TODO: Test: remove the below line and set properly
 --Boder color and width
