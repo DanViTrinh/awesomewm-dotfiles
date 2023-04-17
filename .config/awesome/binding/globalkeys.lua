20f4b347d00c209c92fcdab4fcef7f79554e9647
DanVi007 <danvitrinh@outlook.com>
2023-04-17 20:33:42 +0200

    client resizing

--- .config/awesome/binding/globalkeys.lua
+++ .config/awesome/binding/globalkeys.lua
@@ -180,6 +180,28 @@ function _M.get()
             { description = "focus previous by index", group = "client" }
         ),
 
+        -- CLIENT RESIZING
+        awful.key({ modkey, "Control" }, "j",
+            function(c)
+                resize_client(client.focus, "down")
+            end
+        ),
+        awful.key({ modkey, "Control" }, "k",
+            function(c)
+                resize_client(client.focus, "up")
+            end
+        ),
+        awful.key({ modkey, "Control" }, "h",
+            function(c)
+                resize_client(client.focus, "left")
+            end
+        ),
+        awful.key({ modkey, "Control" }, "l",
+            function(c)
+                resize_client(client.focus, "right")
+            end
+        ),
+
         -- =========================================
         -- NUMBER OF MASTER / COLUMN CLIENTS
         -- =========================================
@@ -220,18 +242,6 @@ function _M.get()
             { description = "quit awesome", group = "awesome" }),
 
         -- Layout manipulation
-        -- awful.key({ modkey, }, "l", function() awful.tag.incmwfact(0.05) end,
-        --     { description = "increase master width factor", group = "layout" }),
-        -- awful.key({ modkey, }, "h", function() awful.tag.incmwfact(-0.05) end,
-        -- { description = "decrease master width factor", group = "layout" }),
-        -- awful.key({ modkey, "Shift" }, "h", function() awful.tag.incnmaster(1, nil, true) end,
-        --     { description = "increase the number of master clients", group = "layout" }),
-        -- awful.key({ modkey, "Shift" }, "l", function() awful.tag.incnmaster(-1, nil, true) end,
-        --     { description = "decrease the number of master clients", group = "layout" }),
-        -- awful.key({ modkey, "Control" }, "h", function() awful.tag.incncol(1, nil, true) end,
-        --     { description = "increase the number of columns", group = "layout" }),
-        -- awful.key({ modkey, "Control" }, "l", function() awful.tag.incncol(-1, nil, true) end,
-        --     { description = "decrease the number of columns", group = "layout" }),
         awful.key({ modkey, }, "space", function() awful.layout.inc(1) end,
             { description = "select next", group = "layout" }),
         awful.key({ modkey, "Shift" }, "space", function() awful.layout.inc(-1) end,
